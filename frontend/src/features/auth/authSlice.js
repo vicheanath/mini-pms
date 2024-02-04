6ed23cd29b431dd831e6e720c5cef7690e2cf080
Vichea <48352653+vicheanath@users.noreply.github.com>
2024-02-04 12:56:22 -0600

    add validatetor zod and react-hook-form

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    isAuthenticated: false,
+    isAuthenticated: true,
     accessToken: null,
     refreshToken: null,
     user: null,
