d54dbec53ad51ed56f6b0a5577504701ff1ed20b
Bunthai Deng <dengbunthai@gmail.com>
2024-02-05 06:23:39 -0600

    file server

--- backend/pms/src/main/java/com/mini/pms/repo/PictureRepo.java
+++ backend/pms/src/main/java/com/mini/pms/repo/PictureRepo.java
@@ -6,7 +6,10 @@ import com.mini.pms.entity.Picture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PictureRepo extends JpaRepository<Picture, Long> {
     //
+    Optional<Picture> findByKey(String key);
 }
---
+++ backend/pms/src/main/java/com/mini/pms/restcontroller/FileRestController.java
@@ -0,0 +1,52 @@
+package com.mini.pms.restcontroller;
+
+import com.mini.pms.service.PictureService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/v1/files")
+@RequiredArgsConstructor
+public class FileRestController {
+
+    private final PictureService picService;
+
+    @PostMapping("upload")
+    public Map upload(MultipartFile file, long propertyId, Principal principal) throws IOException {
+        return Map.of("url", picService.upload(file, propertyId, principal));
+    }
+
+    @GetMapping(
+            value = "/{key}/download",
+            produces = {
+                MediaType.IMAGE_PNG_VALUE,
+                MediaType.IMAGE_JPEG_VALUE,
+                MediaType.IMAGE_GIF_VALUE,
+
+                // MediaType.APPLICATION_OCTET_STREAM_VALUE //will download immediately
+            })
+    public ResponseEntity<Resource> download(@PathVariable("key") String key) throws IOException {
+
+        var fileInfo = picService.download(key);
+        var filename = fileInfo.getPicture().getName();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + filename + "\"")
+                .body(fileInfo.getResource());
+    }
+}
