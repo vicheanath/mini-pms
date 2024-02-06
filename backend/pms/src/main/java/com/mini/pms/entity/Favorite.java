2a2726c485c4d75bf3b78ccfcd8f530c1325d365
Samidinish <doglaszemo@gmail.com>
2024-02-06 12:37:20 -0600

    favtorite impl

---
+++ backend/pms/src/main/java/com/mini/pms/entity/Favorite.java
@@ -0,0 +1,25 @@
+package com.mini.pms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+    
+}
